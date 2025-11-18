1cfd9b31b64ae96b72cde1d80cf911b8b76667d4
renatalavinia175-hub <1059812@senacminas.edu.br>
2025-11-18 17:01:57 -0300

    Foreach

---
+++ exemploForeach.js
@@ -0,0 +1,7 @@
+//Exemplos praticos
+let games = ["Minecraft", "Fortnite", "Roblox"];
+
+//Mostrar cada game com emoji
+games.forEach((game, index) => {
+    console.log(`${index + 1}. ${game}`);
+})
