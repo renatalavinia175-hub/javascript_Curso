d656a6821cf49596e406bf4f3286bb1d1bee575e
renatalavinia175-hub <1059812@senacminas.edu.br>
2025-11-19 15:07:54 -0300

    fix2.js

---
+++ fix2.js
@@ -0,0 +1,11 @@
+let nomes = ["Ana", "Carlos", "Maria"];
+
+nomes.push("Ana");
+nomes.push("Carlos");
+console.log(nomes)
+
+let removido = nomes.pop();
+console.log(removido)
+console.log(nomes)
+
+
