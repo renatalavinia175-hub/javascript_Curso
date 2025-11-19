541df5c49f407decf32ecb0c384509c2d8ebe892
renatalavinia175-hub <1059812@senacminas.edu.br>
2025-11-19 17:04:54 -0300

    exFilter.js

---
+++ exFilter.js
@@ -0,0 +1,8 @@
+let notas = [4.5, 8.0, 6.5, 9.0, 3.0];
+
+let aprovados = notas.filter((nota) =>{
+    return nota >= 7;
+});
+console.log(notas);
+//Aprovados
+console.log(aprovados)
---
+++ fix4.js
@@ -0,0 +1,11 @@
+let prompt = require(`prompt-sync`)({sigint:true})
+let numeros = [5, 15, 8, 25, 3, 18];
+let contador =0;
+
+numeros.forEach(numero => {
+    if (n > 10) {
+        contador++;
+    }
+});
+
+console.log(contador);
