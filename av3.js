0816e29ecdff21d6d480856d71a46f6ec34ac2e2
renatalavinia175-hub <1059812@senacminas.edu.br>
2025-11-25 16:21:26 -0300

    av3.js

--- av3.js
+++ av3.js
@@ -1,10 +1,10 @@
-const prompt = require(`cprompt-sync`)({sigint:true})
+
     
 const usuarios = [                                                         
     {nome: "João", estado: "SP" },
     {nome: "Mariana", estado: "MG" },
-    {nome: "Cauã", estado: "Mg" },
+    {nome: "Cauã", estado: "MG" },
     {nome: "Bianca", estado: "RJ"}
 ];
-const usuariosMG = usuarios.filter(u=>u.estado.includes ("MG"));
-console.log(usuariosMG);
+const lugar = usuarios.filter( u =>u.estado === "MG");
+console.log(lugar);
